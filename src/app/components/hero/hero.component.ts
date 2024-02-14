25c746e379eadd24bd0b35442ad22d01d4d80412
João Ramos <joaoramos13198@gmail.com>
2024-02-14 17:25:17 +0000

    Added banner component

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
