cb2340abc2439cb1625bc7a3f14b71e8cd52962d
João Ramos <joaoramos13198@gmail.com>
2024-03-07 13:39:47 +0000

    Added a json file to simulate the database and added the menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,33 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, HttpClientModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent implements OnInit {
+  products: any;
+  burgers: any[] = [];
+  sides: any[] = [];
+  drinks: any[] = [];
+  activeCategory: string = 'burgers';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('./assets/api/products.json').subscribe((data) => {
+      this.products = data;
+      this.burgers = data.burgers;
+      this.sides = data.sides;
+      this.drinks = data.drinks;
+    });
+  }
+
+  setActiveCategory(category: string): void {
+    this.activeCategory = category;
+  }
+}
